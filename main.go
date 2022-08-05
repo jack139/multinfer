3b903ec88bb5fc15fe96b9357b315eb2ba6c3a41
gt <jack139@gmail.com>
2022-08-05 15:19:20 +0800

    依据配置文件加载模型

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 
 	"github.com/jack139/go-infer/cli"
 	"github.com/jack139/go-infer/types"
+	"github.com/jack139/go-infer/helper"
 
-	//"multinfer/models/detpos"
+	"multinfer/models/detpos"
 	"multinfer/models/bert_qa"
 	"multinfer/models/keras_qa"
 	
@@ -23,11 +24,6 @@ var (
 )
 
 func init() {
-	// 添加模型实例
-	//types.ModelList = append(types.ModelList, &detpos.DetPos{})
-	types.ModelList = append(types.ModelList, &bert_qa.BertQA{})
-	types.ModelList = append(types.ModelList, &keras_qa.AlbertQA{})
-
 	// 命令行设置
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	rootCmd.AddCommand(cli.HttpCmd)
@@ -35,6 +31,26 @@ func init() {
 }
 
 func main() {
+
+	// 根据配置文件 加载模型
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		yaml, _ := cmd.Flags().GetString("yaml")
+		helper.InitSettings(yaml)
+
+		// 添加模型实例
+		if helper.Settings.Customer["Load_Bert_QA"] == "1" {
+			types.ModelList = append(types.ModelList, &bert_qa.BertQA{})
+		}
+		if helper.Settings.Customer["Load_Albert_QA"] == "1" {
+			types.ModelList = append(types.ModelList, &keras_qa.AlbertQA{})
+		}
+		if helper.Settings.Customer["Load_Antigen"] == "1" {
+			types.ModelList = append(types.ModelList, &detpos.DetPos{})
+		}
+
+		return nil
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
